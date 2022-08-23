adf38222c2d7076f07dae9d7b0df80d97a7ec9ac
Ahmed Atwa <ahmedatwa866@yahoo.com>
2022-08-23 23:28:59 +0200

    OpenSearch Component

---
+++ src/components/OpenSearch.js
@@ -0,0 +1,9 @@
+
+export default function OpenSearch() {
+    return (
+        
+        <div className="open-search">
+            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
+        </div>
+    )
+}

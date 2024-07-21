5b2c6bf1532e18f43022e55bdc336ac662f20799
NightKnight <Ahmedatwa866@yahoo.com>
2024-07-21 22:20:58 +0300

    Run cron job hourly

--- netlify/functions/scheduled-function/scheduled-function.ts
+++ netlify/functions/scheduled-function/scheduled-function.ts
@@ -3,9 +3,9 @@ import { open, write } from 'fs'
 
 // To learn about scheduled functions and supported cron extensions,
 // see: https://ntl.fyi/sched-func
-export const handler = schedule('* * * * *', async (event) => {
+export const handler = schedule('@hourly', async (event) => {
   const eventBody = JSON.parse(event.body!)
-  console.log(`Next function run at ${eventBody.next_run}.`)
+  console.log(`Hey there! Next function run at ${eventBody.next_run}.`)
 
   return {
     statusCode: 200,
